6fe5392cbd7681eff82e6b4bf1531dcf4a12312b
BasilGaudion <basil.gaudion@yahoo.fr>
2023-06-01 22:43:09 +0200

    start of responsive tablet for home page

--- src/components/Home/HeroSection/index.js
+++ src/components/Home/HeroSection/index.js
@@ -1,15 +1,22 @@
 import './styles.scss';
-import mobilHeroImg from '../../../assets/images/home/mobile/image-header.jpg';
+import mobilHeroImgMobile from '../../../assets/images/home/mobile/image-header.jpg';
+import mobilHeroImgTablet from '../../../assets/images/home/tablet/image-header.jpg';
+import mobilHeroImgDesktop from '../../../assets/images/home/desktop/image-hero.jpg';
 
 const HeroSection = () => {
   return (
     <section className="hero">
-      <img className="hero--image" src={mobilHeroImg} alt="Xx99 mark 2 Headphones" />
+      <picture>
+        <source srcSet={mobilHeroImgMobile} media="(max-width: 767px)" />
+        <source srcSet={mobilHeroImgTablet} media="(min-width: 768px) and (max-width: 1440px)" />
+        <source srcSet={mobilHeroImgDesktop} media="(min-width: 1024px)" />
+        <img className="hero__image" src={mobilHeroImgMobile} alt="Xx99 mark 2 Headphones" />
+      </picture>
       <div className="hero__container container">
-        <h3 className="hero__container--subtitle">New product</h3>
-        <h2 className="hero__container--title">Xx99 mark ii Headphones</h2>
-        <p className="hero__container--text">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-        <button className="hero__container--button" type="button">See product</button>
+        <h3 className="hero__subtitle">New product</h3>
+        <h2 className="hero__title">Xx99 mark ii Headphones</h2>
+        <p className="hero__text">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+        <button className="hero__button" type="button">See product</button>
       </div>
     </section>
   );
