577d2dc7334edb95a5a7404d4c682ecad78bb463
BasilGaudion <basil.gaudion@yahoo.fr>
2023-06-01 18:33:08 +0200

    component construction productOverview

---
+++ src/components/Home/ProductOverview1/index.js
@@ -0,0 +1,19 @@
+import './styles.scss';
+import speaker from '../../../assets/images/desktop/image-speaker-zx9.png';
+import circle from '../../../assets/icons/circle.svg';
+
+const ProductOverview1 = () => {
+  return (
+    <section className="productoverview1">
+      <div className="productoverview1__container container">
+        <img className="productoverview1__background" src={circle} alt="" />
+        <img className="productoverview1__image" src={speaker} alt="speaker" />
+        <h2 className="productoverview1__title">Zx9 Speaker</h2>
+        <p className="productoverview1__text">Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+        <button className="productoverview1__button" type="button"> See product</button>
+      </div>
+    </section>
+  );
+};
+
+export default ProductOverview1;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import './styles.scss';
 import Header from '../../components/Header';
 import HeroSection from '../../components/Home/HeroSection';
 import Categories from '../../components/Categories';
+import ProductOverview1 from '../../components/Home/ProductOverview1';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header />
       <HeroSection />
       <Categories />
+      <ProductOverview1 />
     </div>
   );
 };
