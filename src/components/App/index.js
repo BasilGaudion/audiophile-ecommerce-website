51fe2239b4b1a225de02677ecb38d80c0c6d485a
BasilGaudion <basil.gaudion@yahoo.fr>
2023-06-02 19:59:07 +0200

    creation of category data and provider setup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,14 @@
 import AppRoutes from '../../utils/AppRoutes';
+import useCategoryProvider, { CategoriesContext } from '../../utils/providers/useCategoriesProvider';
 import './styles.scss';
 
 function App() {
+  const dataCategoryContext = useCategoryProvider();
   return (
     <div className="app">
-      <AppRoutes />
+      <CategoriesContext.Provider value={dataCategoryContext}>
+        <AppRoutes />
+      </CategoriesContext.Provider>
     </div>
   );
 }
