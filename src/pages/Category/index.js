d3b5424598cf2d05224e8d25edfa4f6cf2cd3b74
BasilGaudion <basil.gaudion@yahoo.fr>
2023-06-03 12:38:01 +0200

    dynamic product display by category under construction

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -1,6 +1,7 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import { CategoriesContext } from '../../utils/providers/useCategoriesProvider';
+import { ProductsContext } from '../../utils/providers/useProductsProvider';
 import Header from '../../components/Header';
 import Categories from '../../components/Categories';
 import StoreDescription from '../../components/StoreDescription';
@@ -10,10 +11,22 @@ import ProductListItem from '../../components/CategoryPage/ProductListItem';
 
 const Category = () => {
   const { currentCategory } = useContext(CategoriesContext);
+  const { getProductsByCategory, productsByCategory } = useContext(ProductsContext);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-
+    getProductsByCategory(currentCategory);
   }, [currentCategory]);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [productsByCategory]);
+
+  if (isLoading) {
+    return (
+      <div>isLoading</div>
+    );
+  }
 
   return (
     <div className="category">
--- src/utils/providers/useProductsProvider/index.js
+++ src/utils/providers/useProductsProvider/index.js
@@ -4,11 +4,16 @@ import productsData from './data.json';
 export const ProductsContext = createContext();
 
 const useProductsProvider = () => {
-//   const [currentCategory, setCurrentCategory] = useState();
-//   const [allCategories] = useState(categoriesData.categories);
+  const [productsByCategory, setproductsByCategory] = useState([]);
 
+  const getProductsByCategory = (category) => {
+    const result = productsData.filter(product => product.category === category);
+    console.log("🚀 ~ file: index.js:11 ~ getProductsByCategory ~ result:", result);
+  };
   // on retourne l'objet qu'on passera au provider afin qu'il soit disponible partout
   return {
+    getProductsByCategory,
+    productsByCategory,
 
   };
 };
